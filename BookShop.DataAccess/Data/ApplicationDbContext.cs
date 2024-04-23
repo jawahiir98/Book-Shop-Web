57a9b7d8e802698a74de37b386d770c7a8dfb617
Jawahiir <jawahiirboss@gmail.com>
2024-04-23 18:17:53 +0600

    Order Header + Detail Model, Table

--- BookShop.DataAccess/Data/ApplicationDbContext.cs
+++ BookShop.DataAccess/Data/ApplicationDbContext.cs
@@ -16,6 +16,8 @@ namespace BookShop.DataAccess.Data
         public DbSet<Company> Companies { get; set; }
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }    
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ BookShop.Models/OrderDetail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+
+    }
+}
