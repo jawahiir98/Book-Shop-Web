24c4dbdcddf89161207122062c5db9114b48e382
Jawahiir <jawahiirboss@gmail.com>
2024-04-25 17:35:14 +0600

    Order Management UI

---
+++ BookShopWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public OrderController(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaderList = unitOfWork.OrderHeaders.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+
+    }
+}
