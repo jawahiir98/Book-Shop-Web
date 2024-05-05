279b4801f62e4a13f5433279565b26fe834902b6
Jawahiir <jawahiirboss@gmail.com>
2024-05-05 23:47:30 +0600

    User Role Management

--- BookShopWeb/Areas/Admin/Controllers/UserController.cs
+++ BookShopWeb/Areas/Admin/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using BookShop.Models.ViewModels;
 using BookShop.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -18,17 +19,63 @@ namespace BookShopWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext db;
-        public UserController(ApplicationDbContext _db)
+        private readonly UserManager<IdentityUser> userManager;
+        public UserController(ApplicationDbContext _db,UserManager<IdentityUser> _userManager)
         {
             db = _db;
+            userManager = _userManager;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult RoleManagement(string userId)
+        {
+            var RoleId = db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+            RoleManagementVM roleVM = new RoleManagementVM() {
+                ApplicationUser = db.ApplicationUsers.FirstOrDefault(u => u.Id == userId),
+                RoleList = db.Roles.Select(i=> new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Name
+                }),
+                CompanyList = db.Companies.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+            roleVM.ApplicationUser.Role = db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+            return View(roleVM);
+        }
 
-      
+        [HttpPost]
+        public IActionResult RoleManagement(RoleManagementVM roleManagementVM)
+        {
+            var RoleId = db.UserRoles.FirstOrDefault(u => u.UserId == roleManagementVM.ApplicationUser.Id).RoleId;
+            var oldRole = db.Roles.FirstOrDefault(u => u.Id == RoleId).Name;
+            if (roleManagementVM.ApplicationUser.Role != oldRole)
+            {
+                // Role has been updated. If updated to company then update the company
+                ApplicationUser applicationUser = db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagementVM.ApplicationUser.Id);
+                if(roleManagementVM.ApplicationUser.Role == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = roleManagementVM.ApplicationUser.CompanyId;
+                }
+                if(oldRole == SD.Role_Company)
+                {
+                    applicationUser.CompanyId = null;
+                }
+
+                db.SaveChanges();
+                userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                userManager.AddToRoleAsync(applicationUser, roleManagementVM.ApplicationUser.Role).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+
+
         #region API CALLS
 
         [HttpGet]
